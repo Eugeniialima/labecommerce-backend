489486e4027d7dacd0cf73c1cf0e72327242787b
Maria Eugenia <eugeniialima@gmail.com>
2023-01-26 15:47:41 -0300

    node-package-json

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("O aplicativo foi iniciado!");
+
+const value = process.argv[2]
+
+if (!value) {
+    console.log("digite um argumento");
+}else{
+const result = "bananinha"
+    console.log(result);
+
+
+}
+    
+
+
+
+
